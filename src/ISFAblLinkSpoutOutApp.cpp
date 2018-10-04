ef7e9f5f4c50a9c85e26c817ccadf9489068326e
brucelane <bruce@batchass.fr>
2018-10-04 16:10:58 +0200

    From SDA template, blocks: SDA, Spout, imgui, abllink

---
+++ src/ISFAblLinkSpoutOutApp.cpp
@@ -0,0 +1,193 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+// Settings
+#include "SDASettings.h"
+// Session
+#include "SDASession.h"
+// Log
+#include "SDALog.h"
+// Spout
+#include "CiSpoutOut.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+using namespace SophiaDigitalArt;
+
+class ISFAblLinkSpoutOutApp : public App {
+
+public:
+	ISFAblLinkSpoutOutApp();
+	void mouseMove(MouseEvent event) override;
+	void mouseDown(MouseEvent event) override;
+	void mouseDrag(MouseEvent event) override;
+	void mouseUp(MouseEvent event) override;
+	void keyDown(KeyEvent event) override;
+	void keyUp(KeyEvent event) override;
+	void fileDrop(FileDropEvent event) override;
+	void update() override;
+	void draw() override;
+	void cleanup() override;
+	void setUIVisibility(bool visible);
+private:
+	// Settings
+	SDASettingsRef					mSDASettings;
+	// Session
+	SDASessionRef					mSDASession;
+	// Log
+	SDALogRef						mSDALog;
+	// imgui
+	float							color[4];
+	float							backcolor[4];
+	int								playheadPositions[12];
+	int								speeds[12];
+
+	float							f = 0.0f;
+	char							buf[64];
+	unsigned int					i, j;
+
+	bool							mouseGlobal;
+
+	string							mError;
+	// fbo
+	bool							mIsShutDown;
+	Anim<float>						mRenderWindowTimer;
+	void							positionRenderWindow();
+	bool							mFadeInDelay;
+	SpoutOut 						mSpoutOut;
+};
+
+
+ISFAblLinkSpoutOutApp::ISFAblLinkSpoutOutApp()
+	: mSpoutOut("SDA", app::getWindowSize())
+{
+	// Settings
+	mSDASettings = SDASettings::create();
+	// Session
+	mSDASession = SDASession::create(mSDASettings);
+	//mSDASettings->mCursorVisible = true;
+	setUIVisibility(mSDASettings->mCursorVisible);
+	mSDASession->getWindowsResolution();
+
+	mouseGlobal = false;
+	mFadeInDelay = true;
+	// windows
+	mIsShutDown = false;
+	mRenderWindowTimer = 0.0f;
+	timeline().apply(&mRenderWindowTimer, 1.0f, 2.0f).finishFn([&] { positionRenderWindow(); });
+
+}
+void ISFAblLinkSpoutOutApp::positionRenderWindow() {
+	mSDASettings->mRenderPosXY = ivec2(mSDASettings->mRenderX, mSDASettings->mRenderY);//20141214 was 0
+	setWindowPos(mSDASettings->mRenderX, mSDASettings->mRenderY);
+	setWindowSize(mSDASettings->mRenderWidth, mSDASettings->mRenderHeight);
+}
+void ISFAblLinkSpoutOutApp::setUIVisibility(bool visible)
+{
+	if (visible)
+	{
+		showCursor();
+	}
+	else
+	{
+		hideCursor();
+	}
+}
+void ISFAblLinkSpoutOutApp::fileDrop(FileDropEvent event)
+{
+	mSDASession->fileDrop(event);
+}
+void ISFAblLinkSpoutOutApp::update()
+{
+	mSDASession->setFloatUniformValueByIndex(mSDASettings->IFPS, getAverageFps());
+	mSDASession->update();
+}
+void ISFAblLinkSpoutOutApp::cleanup()
+{
+	if (!mIsShutDown)
+	{
+		mIsShutDown = true;
+		CI_LOG_V("shutdown");
+		// save settings
+		mSDASettings->save();
+		mSDASession->save();
+		quit();
+	}
+}
+void ISFAblLinkSpoutOutApp::mouseMove(MouseEvent event)
+{
+	if (!mSDASession->handleMouseMove(event)) {
+		// let your application perform its mouseMove handling here
+	}
+}
+void ISFAblLinkSpoutOutApp::mouseDown(MouseEvent event)
+{
+	if (!mSDASession->handleMouseDown(event)) {
+		// let your application perform its mouseDown handling here
+		if (event.isRightDown()) { 
+		}
+	}
+}
+void ISFAblLinkSpoutOutApp::mouseDrag(MouseEvent event)
+{
+	if (!mSDASession->handleMouseDrag(event)) {
+		// let your application perform its mouseDrag handling here
+	}	
+}
+void ISFAblLinkSpoutOutApp::mouseUp(MouseEvent event)
+{
+	if (!mSDASession->handleMouseUp(event)) {
+		// let your application perform its mouseUp handling here
+	}
+}
+
+void ISFAblLinkSpoutOutApp::keyDown(KeyEvent event)
+{
+	if (!mSDASession->handleKeyDown(event)) {
+		switch (event.getCode()) {
+		case KeyEvent::KEY_ESCAPE:
+			// quit the application
+			quit();
+			break;
+		case KeyEvent::KEY_h:
+			// mouse cursor and ui visibility
+			mSDASettings->mCursorVisible = !mSDASettings->mCursorVisible;
+			setUIVisibility(mSDASettings->mCursorVisible);
+			break;
+		}
+	}
+}
+void ISFAblLinkSpoutOutApp::keyUp(KeyEvent event)
+{
+	if (!mSDASession->handleKeyUp(event)) {
+	}
+}
+
+void ISFAblLinkSpoutOutApp::draw()
+{
+	gl::clear(Color::black());
+	if (mFadeInDelay) {
+		mSDASettings->iAlpha = 0.0f;
+		if (getElapsedFrames() > mSDASession->getFadeInDelay()) {
+			mFadeInDelay = false;
+			timeline().apply(&mSDASettings->iAlpha, 0.0f, 1.0f, 1.5f, EaseInCubic());
+		}
+	}
+
+	//gl::setMatricesWindow(toPixels(getWindowSize()),false);
+	gl::setMatricesWindow(mSDASettings->mRenderWidth, mSDASettings->mRenderHeight, false);
+	gl::draw(mSDASession->getMixTexture(), getWindowBounds());
+
+	// Spout Send
+	mSpoutOut.sendViewport();
+	getWindow()->setTitle(mSDASettings->sFps + " fps SDA");
+}
+
+void prepareSettings(App::Settings *settings)
+{
+	settings->setWindowSize(640, 480);
+}
+
+CINDER_APP(ISFAblLinkSpoutOutApp, RendererGl, prepareSettings)
